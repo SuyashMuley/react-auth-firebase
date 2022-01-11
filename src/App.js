37327b2ab266868bf540edffb0df8dd49d8eedaf
SuyashMuley <59868486+SuyashMuley@users.noreply.github.com>
2022-01-11 11:56:04 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import firebaseConfig from './firebaseConfig';
 import * as firebaseui from "firebaseui";
 import firebase from "firebase/compat/app";
+// import Home from './Home';
 
 class App extends React.Component {
   componentDidMount() {
-    const fbase = firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
     const uiConfig = {
-      signInSuccessUrl: "https://calculator-qgnbku7zp-suyashmuley.vercel.app", //This URL is used to return to that page when we got success response for phone authentication.
+      signInSuccessUrl: "https://calculator-qgnbku7zp-suyashmuley.vercel.app/", //This URL is used to return to that page when we got success response for phone authentication.
       signInOptions: [firebase.auth.PhoneAuthProvider.PROVIDER_ID],
       tosUrl: "https://calculator-qgnbku7zp-suyashmuley.vercel.app/"
     };
