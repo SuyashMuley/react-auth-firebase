6313582aa877b930a477188cef4442c3960649ab
SuyashMuley <59868486+SuyashMuley@users.noreply.github.com>
2022-01-11 12:41:23 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <h1>REACT PHONE AUTHENTICATION</h1>
       <div id="firebaseui-auth-container"></div>
       </>
     )
